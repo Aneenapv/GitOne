8c37b567c84f94c017bf29947bd03498b80d934e
AneenaPVQA <pvaneena@gmail.com>
2024-10-17 17:41:31 -0400

    git one commit 10/17/24

--- GitOne/src/packageOne/StringPractice.java
+++ GitOne/src/packageOne/StringPractice.java
@@ -21,8 +21,30 @@ public class StringPractice {
 	  char charofString = name.charAt(4);
 	  System.out.println("charofString :"+charofString);
 		
+	  //codePointAt
 	  int codePoint = name.codePointAt(2);
 	  System.out.println("codePoint : "+codePoint);
+	  
+	  int codePointBefore = name.codePointBefore(3);
+	  System.out.println("code point before :"+codePointBefore);
+	  
+	  int codePointCount = name.codePointCount(2, 5);
+	  System.out.println("code point count :"+codePointCount);
+	  
+	  //compareTo
+	  String s1 = "Aneena";
+	  String s2 = "Anee";
+	  
+	  System.out.println(s1.compareTo(s2));
+	  System.out.println(s1.compareToIgnoreCase(s2));
+	  
+	  //concat
+	  
+	  System.out.println("concatenation of strings : "+s1.concat(s2));
+	  
+	  //contains
+	  
+	  System.out.println("contains : "+s1.contains(s2));
 		
 	}
 
